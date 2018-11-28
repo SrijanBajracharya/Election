cac103fe4af6f6b2d478a50334a49dee85b40cb9
Srijan Bajracharya <srijan.bajracharya@gmail.com>
2018-11-28 05:11:13 +0100

    creating a election candidate

---
+++ contracts/Election.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.2;
+
+contract Election{
+	//Model a Candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	//Store candidate
+	//Fetch candidate
+	mapping(uint => Candidate) public candidates;
+	//Store Candidate Count
+	uint public candidatesCount;
+	//Constructor
+	function Election() public{
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string _name) private{
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+	}
+
+}
